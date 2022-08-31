cb2b000dbc9be19289a956e31164e841b00ce015
Luciano Lim <lmlim@cpp.edu>
2022-08-30 22:51:00 -0700

    sets up basic "Hello World" popup

---
+++ src/Index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+
+const rootNode = document.getElementById("app");
+
+if (rootNode) {
+  createRoot(rootNode).render(<App />);
+}
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/Index.tsx",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "script.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./dist"),
+    },
+  },
+  mode: process.env.MODE || "development",
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public" }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"], // add .tsx, .ts
+  },
+};
