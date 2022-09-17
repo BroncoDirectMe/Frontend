64c3cc9dcd8f1db7f5dbbfea70aba1c4a94ec7a3
Leaversa <matttrv.kwong@gmail.com>
2022-09-17 00:02:13 -0700

    adds logic to get professor elements

--- src/Content.ts
+++ src/Content.ts
@@ -1,1 +1,28 @@
+import { createRoot } from 'react-dom/client';
+//import professorPopup from'./ProfessorPopup';
+
+const iframe = document.getElementById('ptifrmtgtframe') as HTMLIFrameElement;
+var insts = iframe.contentWindow?.document.querySelectorAll(
+  '*[id^="MTG_INSTR$"]'
+);
+
+//event listener on the iframe which fires on search
+//since iframe will receive a post message of the search criteria
+//however event will fire regardless if the search criteria is invalid and will remain on the page
+iframe?.contentWindow?.addEventListener('message', (e) => {
+  insts = iframe.contentWindow?.document.querySelectorAll(
+    '*[id^="MTG_INSTR$"]'
+  );
+  console.log(insts);
+
+  if (insts) {
+    insts.forEach((inst) => {
+      //not sure if its a bug but it seems like innerText is invalid but innerHTML works
+      console.log(inst.innerHTML);
+      //null is placeholder for the popup component
+      createRoot(inst).render(null);
+    });
+  }
+});
+
 console.log(document.URL);
