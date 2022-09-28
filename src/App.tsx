be7a21dbe4375a14b947af91253a048164c40e76
Val <valeriedeleon4521@gmail.com>
2022-09-28 14:06:56 -0700

    Revert to button click

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,18 @@ import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { ReactElement } from 'react';
-import { Modal } from './components/Modal/Modal.component';
+
 
 export function App(): ReactElement
 {
-const [isModalOpen, setModalState] = React.useState(false);
-const toggleModal = () => setModalState(!isModalOpen);
-
   return (
     <div>
       <IconButton className='settingsButton'
-          onClick={toggleModal}
+          onClick={() => {
+            alert('clicked');
+          }}
         ><SettingsIcon/>
         </IconButton>
-        <Modal
-          title={"Settings"}
-          isOpen={isModalOpen}
-          onClose={toggleModal}
-        > Insert settings stuff here 
-        </Modal>
       </div>
   );
 }
