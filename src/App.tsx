673f7ff12f150a30a0028dacf5fc68b8ba4d2b60
ALINZ22 <ALANMONGX@GMAIL.COM>
2022-09-28 00:07:57 -0700

    dictionary array and render component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
 import React, { ReactElement } from 'react';
+import ListPage from './ListItem';
+
+const personArray = [
+  {
+    professorName: 'Dr. Billy',
+    overallRating: 4.5,
+    difficulty: 18,
+    reviewCount: 50,
+  },
+  {
+    professorName: 'Professor Bobby',
+    overallRating: 5.0,
+    difficulty: 31,
+    reviewCount: 60,
+  },
+  {
+    professorName: 'Dr. Marshal',
+    overallRating: 1.2,
+    difficulty: 27,
+    reviewCount: 80,
+  },
+  {
+    professorName: 'Dr. J',
+    overallRating: 5,
+    difficulty: 19,
+    reviewCount: 190,
+  },
+  {
+    professorName: 'Professor Yimmy',
+    overallRating: 4.2,
+    difficulty: 18,
+    reviewCount: 800,
+  },
+];
 
 export function App(): ReactElement {
-  return <h1>Hello California</h1>;
+  return <ListPage list={personArray} />;
 }
