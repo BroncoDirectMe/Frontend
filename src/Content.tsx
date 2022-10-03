7e34ecc0804f29cfb0a74f969c7321059f15d196
Lina Kang <33437092+dal07065@users.noreply.github.com>
2022-10-02 21:58:33 -0700

    Leaf/blake/identify broncodirect page (#36)
    * Identify BroncoDirect Page, Class Search, Search Results Pages
    
    * Fixed Console Log Formatting Edits
    
    * Additional Edits to Fix Merging
    
    * Update Content.tsx
    
    * Prettier Readme.md Fix
    
    * Update src/Content.tsx
    
    Co-authored-by: Matthew Kwong <mtkwong@cpp.edu>
    
    * Update Content.tsx
    
    * Identify page by title, not list of professors. Reset manifest.json
    
    * Added documentation
    
    * Added Documentation with signing key
    
    * Verified Commit Test
    
    * Verified Commit Test 2
    
    * Linting Issues
    
    * Update Content.tsx
    
    Co-authored-by: Matthew Kwong <mtkwong@cpp.edu>

--- src/Content.tsx
+++ src/Content.tsx
@@ -2,27 +2,46 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import ProfessorPopup from './ProfessorPopup';
 
-const iframe = document.getElementById('ptifrmtgtframe') as HTMLIFrameElement;
+if (document.URL.includes('https://cmsweb.cms.cpp.edu/'))
+  console.log('BroncoDirect Page Loaded');
 
-// event listener on the iframe which fires on search
-// since iframe will receive a post message of the search criteria
-// however event will fire regardless if the search criteria is invalid and will remain on the page
-iframe?.contentWindow?.addEventListener('message', () => {
-  const insts: NodeListOf<HTMLElement> | undefined =
-    iframe.contentWindow?.document.querySelectorAll('*[id^="MTG_INSTR$"]');
-  // iterate through insts and create new instance of ProfessorPopup for each inst
-  insts?.forEach((inst) => {
-    // append a new root container under inst.parent to retain original span element
-    const root = document.createElement('div');
-    const parentElem = inst.parentElement as HTMLDivElement;
+if (
+  document.URL.includes(
+    'https://cmsweb.cms.cpp.edu/psp/CPOMPRDM/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL?'
+  )
+) {
+  const iframe = document.getElementById('ptifrmtgtframe') as HTMLIFrameElement;
 
-    // styling for the ProfessorPopup Button
-    root.setAttribute('id', 'root');
-    root.style.float = 'right';
-    parentElem?.append(root);
+  // event listener on the iframe which fires on search
+  // since iframe will receive a post message of the search criteria
+  // however event will fire regardless if the search criteria is invalid and will remain on the page
+  iframe?.contentWindow?.addEventListener('message', () => {
+    const insts: NodeListOf<HTMLElement> | undefined =
+      iframe.contentWindow?.document.querySelectorAll('*[id^="MTG_INSTR$"]');
 
-    createRoot(root).render(<ProfessorPopup professorName={inst.innerText} />);
+    const header: HTMLElement | null | undefined =
+      iframe.contentWindow?.document.querySelector('.gh-page-header-headings');
+
+    // Log to console the current page of Bronco Direct (using the header of the page)
+    if (header !== undefined && header !== null)
+      console.log((header.children[2] as HTMLElement).innerText);
+    else console.log('undefined');
+
+    // iterate through insts and create new instance of ProfessorPopup for each inst
+    insts?.forEach((inst) => {
+      // append a new root container under inst.parent to retain original span element
+      const root = document.createElement('div');
+      const parentElem = inst.parentElement as HTMLDivElement;
+
+      // styling for the ProfessorPopup Button
+      root.setAttribute('id', 'root');
+      root.style.float = 'right';
+      parentElem?.append(root);
+
+      createRoot(root).render(
+        <ProfessorPopup professorName={inst.innerText} />
+      );
+    });
   });
-});
-
+}
 console.log(document.URL);
