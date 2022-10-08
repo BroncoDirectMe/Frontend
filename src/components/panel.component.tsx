84c07997b0c2418d7710c354082495bb9dbfafa6
Val <valeriedeleon4521@gmail.com>
2022-10-07 22:01:11 -0700

    whitespace changes

--- src/components/panel.component.tsx
+++ src/components/panel.component.tsx
@@ -11,18 +11,15 @@ interface PanelProps
 
 export const Panel: React.FC<PanelProps> = ({title, isOpen, onClose, children}) => isOpen ?
 (
-<div className={'panel'}>
+    <div className={'panel'}>
         <div className={'title'}>
             {title}
         </div>
         <div className={'content'}>
             {children}
-
         // Appearance & Design
         <Button className={'closeBtn'} 
             onClick={onClose}></Button>
-
-
         </div>
     </div>
 ) : null;
