e65e941612c4a2756d5e43ebbf9797fb24a36559
GuyWhoCode <thatguywhocodes@gmail.com>
2022-11-17 00:05:27 -0800

    Added email validation and error elm

--- src/MicrosoftOath.tsx
+++ src/MicrosoftOath.tsx
@@ -4,7 +4,14 @@ import {
   PublicClientApplication,
   RedirectRequest,
 } from '@azure/msal-browser';
-import React from 'react';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import Alert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import CloseIcon from '@mui/icons-material/Close';
+import { Box } from '@mui/system';
+import { App } from './App';
 
 const redirectUri =
   typeof chrome !== 'undefined' && chrome.identity
@@ -24,7 +31,16 @@ const msalConfig: Configuration = {
   },
 };
 
-export const msalInstance = new PublicClientApplication(msalConfig);
+export const msalInstance: PublicClientApplication =
+  new PublicClientApplication(msalConfig);
+
+/**
+ * Determines whether user has a CPP account
+ * @param email String
+ * @returns True if the email is valid. False otherwise.
+ */
+const validateEmail = (email: string | undefined): boolean =>
+  email?.split('@')[1].includes('cpp.edu') ?? false;
 
 /**
  * Launches Microsoft Authentication through Google Chrome Extension Authentication function (sample code from MSAL)
@@ -39,7 +55,7 @@ async function launchWebAuthFlow(
         interactive: true,
         url,
       },
-      (responseUrl) => {
+      (responseUrl: string | undefined) => {
         if (responseUrl?.includes('#')) {
           msalInstance
             .handleRedirectPromise(`#${responseUrl.split('#')[1]}`)
@@ -56,28 +72,6 @@ async function launchWebAuthFlow(
 }
 
 /**
- * Returns the user sign into the browser.
- */
-// async function getSignedInUser(): Promise<chrome.identity.UserInfo | null> {
-//   return await new Promise((resolve) => {
-//     if (chrome?.identity) {
-//       // Running in extension popup
-//       chrome.identity.getProfileUserInfo((user) => {
-//         if (user) {
-//           resolve(user);
-//         } else {
-//           resolve(null);
-//         }
-//       });
-//     } else {
-//       // Running on localhost
-//       resolve(null);
-//     }
-//   });
-// }
-
-/**
- *
  * @param request RedirectRequest specifying the scope of the login (what information should be obtained)
  * @returns URL that actually redirects user to Microsoft page to input login credentials
  */
@@ -96,21 +90,88 @@ async function getLoginUrl(request: RedirectRequest): Promise<string> {
 }
 
 /**
+ * Creates Login Error element in the extension window using MUI
+ */
+function LoginErrorElement(): JSX.Element {
+  const [open, setOpen] = React.useState(true);
+
+  return (
+    <Collapse in={open}>
+      <Alert
+        action={
+          <IconButton
+            aria-label="close"
+            color="inherit"
+            size="small"
+            onClick={() => {
+              setOpen(false);
+            }}
+          >
+            <CloseIcon fontSize="inherit" />
+          </IconButton>
+        }
+        sx={{ mb: 2 }}
+        variant="outlined"
+        severity="error"
+      >
+        Error: Microsoft Account not associated with CPP.
+      </Alert>
+    </Collapse>
+  );
+}
+
+/**
  * Initiates Microsoft Authentication. Use this function to prompt users to login
  */
 async function signIn(): Promise<void> {
   const signInRequest: RedirectRequest = {
     scopes: ['user.read'],
   };
-  const url = await getLoginUrl(signInRequest);
-  const result = await launchWebAuthFlow(url);
+  const url: string = await getLoginUrl(signInRequest);
+  try {
+    const result: AuthenticationResult | null = await launchWebAuthFlow(url);
+    // Launches Google Chrome extension's Popup window where the user can login
 
-  const accessToken = result?.accessToken;
-  const userEmail = result?.account?.username;
-  console.log(accessToken);
-  console.log(userEmail);
+    const accessToken: string | undefined = result?.accessToken;
+    const userEmail: string | undefined = result?.account?.username;
+
+    if (validateEmail(userEmail)) {
+      await fetch('/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify({ accessToken }),
+      });
+      // POST request to the backend with endpoint /user
+
+      return await new Promise((resolve) => resolve());
+      // Empty promise to make guard clause work
+    }
+
+    const currentAccount = msalInstance.getAccountByHomeId(
+      result?.account?.homeAccountId ?? 'homeID'
+    );
+    await msalInstance.logoutPopup({ account: currentAccount });
+    // Enables prompt-less logout -- Microsoft Authentication will force the user to sign out
+
+    const appContainer = document.getElementById('app') as HTMLElement;
+    createRoot(appContainer).render(
+      <Box>
+        <LoginErrorElement />
+        <App />
+      </Box>
+    );
+  } catch {
+    console.log('The login interaction failed.');
+    sessionStorage.clear();
+    // Clears session storage to allow user to open login popup in the same extension window
+  }
 }
 
+/**
+ * Creates Sign in Button element that triggers a pop up window prompting users to login with their Microsoft Account
+ */
 export function MicrosoftOAuth(): JSX.Element {
   return (
     <button
