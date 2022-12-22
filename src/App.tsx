5b75e26638bdc53e4035b22aafc5139ceed68440
GuyWhoCode <thatguywhocodes@gmail.com>
2022-12-22 15:43:26 -0800

    Added search result rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,8 @@
 import React, { ReactElement } from 'react';
-import ListPage from './ListItem';
 import { ToggleButton } from './ToggleButton';
 import SearchBar from './SearchBar';
 import { MsalProvider } from '@azure/msal-react';
 import { msalInstance, MicrosoftOAuth } from './MicrosoftOath';
-
-const personArray = [
-  {
-    professorName: 'Dr. Billy',
-    overallRating: 4.5,
-    difficulty: 18,
-    reviewCount: 50,
-  },
-  {
-    professorName: 'Professor Bobby',
-    overallRating: 5.0,
-    difficulty: 31,
-    reviewCount: 60,
-  },
-  {
-    professorName: 'Dr. Marshal',
-    overallRating: 1.2,
-    difficulty: 27,
-    reviewCount: 80,
-  },
-  {
-    professorName: 'Dr. J',
-    overallRating: 5,
-    difficulty: 19,
-    reviewCount: 190,
-  },
-  {
-    professorName: 'Professor Yimmy',
-    overallRating: 4.2,
-    difficulty: 18,
-    reviewCount: 12,
-  },
-];
 
 export function App(): ReactElement {
   return (
@@ -44,7 +10,6 @@ export function App(): ReactElement {
       <div className="App">
         <ToggleButton />
         <SearchBar />
-        <ListPage list={personArray} />
         <MicrosoftOAuth />
       </div>
     </MsalProvider>
--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,37 +1,90 @@
 import { TextField } from '@mui/material';
-import React from 'react';
+import React, { CSSProperties, useState, useEffect } from 'react';
+import { ListPage, person } from './ListItem';
+
+let searchView: CSSProperties = {
+  display: 'none',
+};
+let noResultsView: CSSProperties = {
+  display: 'none',
+};
 
 export default function SearchBar(): JSX.Element {
-  const [searchText, setsearchText] = React.useState('');
+  const [searchText, setsearchText] = useState('');
+  const [count, hasResult] = useState(0);
+  const [searchResult, newResult] = useState<person>({
+    professorName: 'professor',
+    overallRating: 1.0,
+    difficulty: 1.0,
+    reviewCount: 1,
+  });
+
+  useEffect(() => {
+    noResultsView = {
+      display: 'block',
+    };
+    searchView = {
+      display: 'none',
+    };
+  }, [count])
+  // attempt at trying to render a 'no results' element when the search has no results
 
   return (
-    <>
-      <div>
-        <h1>BroncoDirectMe Search Bar</h1>
-        <TextField
-          type="text"
-          value={searchText}
-          fullWidth
-          onChange={(e) => {
-            setsearchText(e.target.value);
-          }}
-          // eslint-disable-next-line @typescript-eslint/no-misused-promises
-          onKeyUp={async (e) => {
-            if (e.key === 'Enter') {
+    <div>
+      <h1>BroncoDirectMe Search Bar</h1>
+      <TextField
+        type="text"
+        value={searchText}
+        fullWidth
+        onChange={(e) => {
+          setsearchText(e.target.value);
+        }}
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onKeyUp={async (e) => {
+          if (e.key === 'Enter') {
+            try {
               const request = await fetch('http://localhost:3000/professor', {
                 method: 'POST',
                 headers: {
                   'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ test: 0 }),
+                body: JSON.stringify({ name: searchText }),
               });
               // POST request to the backend with endpoint /professor
-              const response = await request.json();
-              console.log(response)
+              const {
+                avgRating,
+                avgDifficulty,
+                firstName,
+                lastName,
+                numRatings,
+              } = await request.json();
+
+              searchView = {
+                display: 'block',
+              };
+              noResultsView = {
+                display: 'none',
+              };
+              newResult({
+                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                professorName: `${firstName} ${lastName}`,
+                overallRating: avgRating,
+                difficulty: avgDifficulty,
+                reviewCount: numRatings,
+              });
+            } catch {
+              console.log("no work")
+              hasResult((count) => count ++)
+              // Case when the search yields no results
             }
-          }}
-        />
-      </div>
-    </>
+          }
+        }}
+      />
+
+      <section id="searchResult" style={searchView}>
+        <ListPage list={[searchResult]} />
+      </section>
+      <p style={noResultsView}>The query yielded no results</p>
+    </div>
   );
 }
