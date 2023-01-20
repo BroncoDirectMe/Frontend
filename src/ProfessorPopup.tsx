af9c19a18ca17477631a7e7815cae29ad4f02ccc
PattyCakesxD <nguyenpatty42@gmail.com>
2023-01-20 14:47:54 -0800

    now uses ProfessorNameFiltering function

--- src/ProfessorPopup.tsx
+++ src/ProfessorPopup.tsx
@@ -139,7 +139,7 @@ function ProfessorPopupInfo(props: professorPopupTooltipProps): JSX.Element {
         <Typography
           style={{ fontSize: '1.5rem', fontWeight: 'bold', color: '#008970' }}
         >
-          {props.professorName.toUpperCase()}
+          {ProfessorNameFiltering(props.professorName.toUpperCase())}
         </Typography>
       </Paper>
       <Divider style={{ margin: '10px 0' }} />
