51a585336f8eadf3bc88ac45741ce3d233c49434
Leaversa <matttrv.kwong@gmail.com>
2023-04-03 15:59:35 -0700

    eslint fixes

--- src/Content.tsx
+++ src/Content.tsx
@@ -3,14 +3,15 @@ import { createRoot } from 'react-dom/client';
 
 const observer = new MutationObserver(function (mutationList) {
   mutationList.forEach((mutation) => {
-    //runs if data-subpage attribute in document.body changes
-    if (mutation.attributeName == 'data-subpage') {
-      let currPage = (mutation.target as HTMLElement).getAttribute(
+    // runs if data-subpage attribute in document.body changes
+    if (mutation.attributeName === 'data-subpage') {
+      const currPage = (mutation.target as HTMLElement).getAttribute(
         'data-subpage'
       );
       console.log('[BRONCODIRECT] Current Page:', currPage);
 
       // example injection
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       const insts = (
         document.getElementById('ptifrmtgtframe') as HTMLIFrameElement
       ).contentDocument!.querySelectorAll('*[id^="MTG_INSTR$"]');
@@ -20,7 +21,7 @@ const observer = new MutationObserver(function (mutationList) {
 });
 
 // example injection
-function testInject(insts: NodeListOf<Element>) {
+function testInject(insts: NodeListOf<Element>): void {
   insts?.forEach((inst) => {
     const TESTINJECTION = document.createElement('div');
     const parentElem = inst.parentElement as HTMLDivElement;
@@ -35,7 +36,7 @@ function testInject(insts: NodeListOf<Element>) {
 
 function pageLoadCheck(): boolean {
   // URL set on entire cpp portal so injection will work on add class page too
-  const URL = 'https://cmsweb.cms.cpp.edu/psp/'; ///psp/CPOMPRDM/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL?';
+  const URL = 'https://cmsweb.cms.cpp.edu/psp/'; // /CPOMPRDM/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL?';
   if (!window.location.href.includes(URL)) return false;
   console.log('[BRONCODIRECT] Page Loaded');
   return true;
