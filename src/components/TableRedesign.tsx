bd4b4877eef3f6bb3a59725b9b48022ca470adbc
Leaversa <matttrv.kwong@gmail.com>
2023-04-06 02:25:27 -0700

    cleaned up courseScraping and added comments

--- src/components/TableRedesign.tsx
+++ src/components/TableRedesign.tsx
@@ -7,32 +7,34 @@ import {
   TableRow,
 } from '@mui/material';
 
-const tableScraper = (arr: any) => {
-  return Array.from(arr).map((i: any) =>
-    Array.from(i.querySelectorAll('span'))
-      .map((i: any) => i.innerText)
-      .filter((i) => i !== '')
+// returns Course array of array class details
+function tableScraper(classRows: NodeListOf<HTMLElement>): string[][] {
+  return Array.from(classRows).map((rowContent: Element) => {
+    const rowSpanElems = rowContent.querySelectorAll('span');
+    const rowTextContents = Array.from(rowSpanElems).map((i) => i.innerText);
+    return rowTextContents.filter((i) => i !== ''); // remove blank entries
+  });
+}
+
+function detailsMapping(elem: Element): object {
+  const classSections: NodeListOf<HTMLElement> = elem.querySelectorAll(
+    '*[id^="trSSR_CLSRCH_MTG1$"]'
   );
-};
-const detailsMapping = (elem: any) =>
-  tableScraper(elem.querySelectorAll('*[id^="trSSR_CLSRCH_MTG1$"]')).map(
-    ([ID, Section, Times, Location, Instr, Dates, Status]) => ({
-      ID,
-      Section,
-      Times,
-      Location,
-      Instr,
-      Dates,
-      Status,
-    })
-  );
-const DataMapping = (fold: any) => {
-  const map: any = {};
-  Array.from(fold).map((elem: any) => {
-    map[elem.querySelectorAll('h2,h3,h4')[0].innerText] = detailsMapping(elem);
+  return tableScraper(classSections).map((classSection) => {
+    const [ID, Section, Times, Location, Instr, Dates, Status] = classSection;
+    return { ID, Section, Times, Location, Instr, Dates, Status };
+  });
+}
+// maps array of class sections to object value and course name as object key
+function DataMapping(classRows: NodeListOf<Element>): Record<string, object> {
+  const map: Record<string, object> = {};
+  Array.from(classRows).forEach((elem) => {
+    const courseTitle = elem.querySelectorAll('h2,h3,h4')[0] as HTMLElement; // first instance of a heading
+
+    map[courseTitle.innerText] = detailsMapping(elem);
   });
   return map;
-};
+}
 
 type prop = {
   classRows: NodeListOf<HTMLElement>;
