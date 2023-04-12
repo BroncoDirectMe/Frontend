e22c016a8af3bdf7f0bb6e75b409c0dda541076f
Ali Momennasab <81054872+alimomennasab@users.noreply.github.com>
2023-04-11 23:47:20 -0700

    Leaf/alimomennasab/redesign professor popup window (#87)
    * Changed values in popup window to be circular progress bars, added retention values to the popup, various reformatting and resizing
    
    * Added circular progress bar function, aligned progress bars horizontally
    
    * Visual improvements- color changes, resizing, realigning
    
    * - Fixed bug where search bar deletes its contents after pressing enter. Also some visual improvements
    
    * Prettier format
    
    * Small visual improvements
    
    * Prettier format
    
    * More visual improvements
    
    * eslint fixes
    
    * centers input in search bar
    
    * fix retention basecase
    
    * temporary fix to header alignment
    
    * temporarily disables MicrosoftOAuth
    
    * updates failed query message
    
    * Fixed header alignment and added RMP button
    
    * Prettier fix
    
    * Aligned search bar left
    
    * Fixed edge case showing default value
    
    ---------
    
    Co-authored-by: Patrick Hoang <nguyenpatty42@gmail.com>
    Co-authored-by: GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React, { ReactElement } from 'react';
 import { ToggleButton } from './ToggleButton';
 import SearchBar from './SearchBar';
 import { MsalProvider } from '@azure/msal-react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { msalInstance, MicrosoftOAuth } from './MicrosoftOath';
-import { Grid, IconButton, Typography } from '@mui/material';
+import { Box, IconButton } from '@mui/material';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { Panel } from './components/panel_component';
 
@@ -18,28 +19,27 @@ export function App(): ReactElement {
         {!isPanelOpen && (
           <section>
             <div id="errorElm"></div>
-            <Grid container>
-              <Grid item xs={11}>
-                <Typography fontSize="1.5rem" fontWeight="600">
-                  BroncoDirectMe
-                </Typography>
-              </Grid>
-              <Grid item xs={1} style={{ display: 'flex' }}>
-                {isSettingsButtonOpen && (
-                  <IconButton
-                    onClick={() => {
-                      togglePanel();
-                      setSettingsButtonState(false);
-                    }}
-                    sx={{ padding: '0' }}
-                  >
-                    <SettingsIcon sx={{ fontSize: '2rem' }} />
-                  </IconButton>
-                )}
-              </Grid>
-            </Grid>
-            <SearchBar />
-            <MicrosoftOAuth />
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              paddingLeft="5vw"
+              paddingRight="5vw"
+            >
+              <h1>BroncoDirectMe Search</h1>
+              {isSettingsButtonOpen && (
+                <IconButton
+                  onClick={() => {
+                    togglePanel();
+                    setSettingsButtonState(false);
+                  }}
+                  sx={{ padding: '0' }}
+                >
+                  <SettingsIcon sx={{ fontSize: '2rem' }} />
+                </IconButton>
+              )}
+            </Box>
+            <SearchBar settingBarState={isSettingsButtonOpen} />
+            {/* <MicrosoftOAuth /> */}
           </section>
         )}
         {/* Hides main app components when setting panel opens */}
