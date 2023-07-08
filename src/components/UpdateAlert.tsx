12eed9f6a36e7f72ca5ce9b2d2d245877453811c
Minh Tran <mtran8011@yahoo.com>
2023-07-07 18:07:15 -0700

    Fix: eslint errors

--- src/components/UpdateAlert.tsx
+++ src/components/UpdateAlert.tsx
@@ -1,12 +1,10 @@
-/*global chrome*/
 import React, { useState, useEffect } from 'react';
-import { Alert, Button } from '@mui/material';
+import { Alert } from '@mui/material';
 
 const UpdateAlert = (): JSX.Element => {
   const [alertOpen, setAlertOpen] = useState(false);
 
   useEffect(() => {
-    const str = chrome.runtime.getURL;
     // calls function when update is available
     chrome.runtime.onUpdateAvailable.addListener(handleUpdateAvailable);
   }, []);
@@ -16,22 +14,13 @@ const UpdateAlert = (): JSX.Element => {
   };
 
   const handleCloseAlert = (): void => {
-    // updates the extension when it reloads, see onUpdateAvailable docs
-    chrome.runtime.reload;
     setAlertOpen(false);
   };
 
   // The Alert itself
   return alertOpen ? (
-    <Alert
-      action={
-        <Button onClick={handleCloseAlert} color="inherit" size="small">
-          UPDATE
-        </Button>
-      }
-      severity="info"
-    >
-      A new version is available!
+    <Alert onClose={handleCloseAlert} severity="info">
+      A new version is available! Please update.
     </Alert>
   ) : (
     <></>
