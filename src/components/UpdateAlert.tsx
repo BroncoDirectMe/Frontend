8eba3f4ddac4f65edbef1c2b1add9dbb6c58056a
Minh Tran <mtran8011@yahoo.com>
2023-07-15 16:38:23 -0700

    Allow alert to reappear next update available

--- src/components/UpdateAlert.tsx
+++ src/components/UpdateAlert.tsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Alert } from '@mui/material';
 
 const UpdateAlert = (): JSX.Element => {
-  const [alertVisible, setAlertVisible] = useState(false);
+  const [alertVisible, setAlertVisible] = useState(true);
 
   useEffect(() => {
-    // calls function when update is available
     chrome.runtime.onUpdateAvailable.addListener(handleUpdateAvailable);
+    chrome.runtime.onInstalled.addListener(handleResetAlertClosedStatus);
 
     // does not appear again after closing update notif
     chrome.storage.local.get('alertClosed', ({ alertClosed }) => {
@@ -18,7 +18,10 @@ const UpdateAlert = (): JSX.Element => {
 
   const handleUpdateAvailable = (): void => {
     setAlertVisible(true);
-    console.log(`Updated | alertClosed set to false`);
+  };
+
+  // the 'closed forever' status of the alert is reset when extension is updated
+  const handleResetAlertClosedStatus = (): void => {
     chrome.storage.local.set({ alertClosed: false }, () => {
       if (chrome.runtime.lastError) {
         console.error(chrome.runtime.lastError);
@@ -26,8 +29,8 @@ const UpdateAlert = (): JSX.Element => {
     });
   };
 
+  // user closes the alert
   const handleCloseAlert = (): void => {
-    console.log(`Alert closed! | alertClosed set to true`);
     setAlertVisible(false);
     chrome.storage.local.set({ alertClosed: true }, () => {
       if (chrome.runtime.lastError) {
