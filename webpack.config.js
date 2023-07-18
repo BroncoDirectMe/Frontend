7500143a3d0db74650ed3ed4508141892a311a39
Valen DeLeon <valeriedeleon4521@gmail.com>
2023-07-18 14:01:24 -0700

    removes dups in webpack, installs copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,7 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: {
-    script: './src/Index.tsx',
-    content: './src/Content.tsx',
-  },
   entry: {
     script: './src/Index.tsx',
     content: './src/Content.tsx',
@@ -15,20 +9,15 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].js',
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].js',
   },
   devServer: {
     static: {
       directory: path.resolve(__dirname, './dist'),
-      directory: path.resolve(__dirname, './dist'),
     },
   },
   mode: process.env.MODE || 'development',
-  mode: process.env.MODE || 'development',
   plugins: [
     new CopyWebpackPlugin({
-      patterns: [{ from: 'public' }],
       patterns: [{ from: 'public' }],
     }),
   ],
@@ -51,7 +40,6 @@ module.exports = {
         test: /\.(js|ts)x?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
           loader: 'babel-loader',
         },
       },
